7816fdb742cb3910441bc6bcdcbf1caff20988c5
Jaime Hansen <jaimehansenfilho@gmail.com>
2025-02-19 21:25:21 -0300

    Ajutes server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const path = require("path");
-const http = require("http");
-const { Server } = require("socket.io");
+const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 const port = 3000;
@@ -9,24 +9,30 @@ const port = 3000;
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.resolve("")));
+app.use(express.static(path.resolve('')));
 
-io.on("connection", (socket) => {
-    console.log("Connected");
+io.on('connection', (socket) => {
+  console.log('Client connected');
 
-    socket.on("right", (e) => {
-        console.log(e);
-    });
+  socket.on('right', (data) => {
+    console.log('Recebido:', data);
+    socket.emit('right', 'You moved to the right!');
+  });
 
-    socket.on("left", (e) => {
-        console.log(e);
-    });
+  socket.on('left', (data) => {
+    console.log('Recebido:', data);
+    socket.emit('teste_left', 'You moved to the left!');
+  });
 
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
+  socket.on('stop', (data) => {
+    console.log('Stop event:', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
 });
 
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
