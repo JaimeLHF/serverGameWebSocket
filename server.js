b60d6d13f157def7f093e2997af27e656ae56d35
Jaime Hansen <jaimehansenfilho@gmail.com>
2025-02-26 19:30:20 -0300

    Ajusted II

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const rooms = {};
 
 const PORT = 3000;
 
@@ -15,10 +16,30 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('Novo cliente conectado');
 
-    socket.on('move', (direction) => {
-        console.log(`Direção: ${direction}`);
-        io.emit('direction', direction);
-    })
+    socket.on('create-room', () => {
+        const roomId = Math.random().toString(36).substring(2, 8); // ID aleatório
+        rooms[roomId] = { controller: null, game: null };
+        socket.emit('room-created', roomId);
+        console.log(`Sala criada: ${roomId}`);
+    });
+
+    socket.on('join', ({ roomId, role }) => {
+        if (rooms[roomId]) {
+            rooms[roomId][role] = socket;
+            socket.roomId = roomId;
+            console.log(`${role} entrou na sala ${roomId}`);
+        }
+    });
+
+    socket.on('move', (data) => {
+        if (!socket.roomId) return;
+        const room = rooms[socket.roomId];
+
+        if (room && room.game) {
+            room.game.emit('direction', data);
+            console.log(`Movimento enviado para game: ${data}`);
+        }
+    });
 
     socket.on('disconnect', () => {
         console.log('Cliente desconectado');
